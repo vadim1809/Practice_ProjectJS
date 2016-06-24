3863659dbe6f8c0c8948336ed7b1248394001d12
Вадим Томащук < >
2016-06-24 03:18:05 +0300

    Simplest design

--- js/tanks.js
+++ js/tanks.js
@@ -236,7 +236,7 @@ function preload () {
     game.load.atlas('enemy', 'assets/enemy-tanks.png', 'assets/tanks.json');
     game.load.image('logo', 'assets/logo.png');
     game.load.image('bullet', 'assets/bullet.png');
-    game.load.image('earth', 'assets/scorched_earth.png');
+    game.load.image('earth', 'assets/light_grass.png');
     game.load.spritesheet('kaboom', 'assets/explosion.png', 64, 64, 23);
     
 }
